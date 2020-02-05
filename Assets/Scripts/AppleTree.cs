a67644174cf3940219adc7ab98ad7d13fc6a7587
megafeebas <55175492+megafeebas@users.noreply.github.com>
2020-02-05 18:48:40 -0500

    Started AppleTree Script

--- Assets/Scripts/AppleTree.cs
+++ Assets/Scripts/AppleTree.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class AppleTree : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject applePrefab;
+    public float speed = 1f;
+    public float leftAndRightEdge = 10f;
+    public float chanceToChangeDirection = 0.1f;
+    public float secondsBetweenAppleDrops = 1f;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
+       
     }
 }
