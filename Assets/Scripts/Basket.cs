233b0f0e7abca53dd52fa479fd68d48b50e90533
megafeebas <55175492+megafeebas@users.noreply.github.com>
2020-02-05 19:22:17 -0500

    Apple Collision

--- Assets/Scripts/Basket.cs
+++ Assets/Scripts/Basket.cs
@@ -20,4 +20,12 @@ public class Basket : MonoBehaviour
         pos.x = mousePos3D.x;
         this.transform.position = pos;
     }
+    void OnCollisionEnter(Collision coll)
+    {
+        GameObject collidedWith = coll.gameObject;
+        if(collidedWith.tag == "Apple")
+        {
+            Destroy(collidedWith);
+        }
+    }
 }
