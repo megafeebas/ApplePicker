e063e326a1c5a21624504306fa311779d565cc4f
megafeebas <55175492+megafeebas@users.noreply.github.com>
2020-02-05 19:35:50 -0500

    Score and Apple Destroyed

--- Assets/Scripts/Basket.cs
+++ Assets/Scripts/Basket.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class Basket : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Text scoreGT;
+
     void Start()
     {
-        
+        GameObject scoreGO = GameObject.Find("ScoreCounter");
+        scoreGT = scoreGO.GetComponent<Text>();
+        scoreGT.text = "0";
     }
 
     // Update is called once per frame
@@ -26,6 +29,9 @@ public class Basket : MonoBehaviour
         if(collidedWith.tag == "Apple")
         {
             Destroy(collidedWith);
+            int score = int.Parse(scoreGT.text);
+            score += 100;
+            scoreGT.text = score.ToString();
         }
     }
 }
