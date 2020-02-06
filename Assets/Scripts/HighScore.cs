175dc6738f2c43bca395686bf9bd09ab06a921e5
megafeebas <55175492+megafeebas@users.noreply.github.com>
2020-02-05 20:07:48 -0500

    Slightly Increased Difficulty

--- Assets/Scripts/HighScore.cs
+++ Assets/Scripts/HighScore.cs
@@ -6,7 +6,15 @@ using UnityEngine.UI;
 public class HighScore : MonoBehaviour
 {
     static public int score = 1000;
-    // Start is called before the first frame update
+    
+    void Awake()
+    {
+        if (PlayerPrefs.HasKey("HighScore"))
+        {
+            score = PlayerPrefs.GetInt("HighScore");
+        }
+        PlayerPrefs.SetInt("HighScore", score);
+    }
     void Start()
     {
         
@@ -17,5 +25,9 @@ public class HighScore : MonoBehaviour
     {
         Text gt = this.GetComponent<Text>();
         gt.text = "High Score: " + score;
+        if(score > PlayerPrefs.GetInt("HighScore"))
+        {
+            PlayerPrefs.SetInt("HighScore", score);
+        }
     }
 }
